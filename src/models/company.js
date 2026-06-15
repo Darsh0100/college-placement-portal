306c62dcd5197510ef5619413c62f9d2bd707762
YasharthTri <yasharth1312@gmail.com>
2026-06-15 15:20:21 +0530

    company model created

---
+++ src/models/company.js
@@ -0,0 +1,37 @@
+const mongoose=require("mongoose");
+
+const CompanySchema=new mongoose.Schema({
+    CompanyName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+    CompanyEmail:{
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    companyWebsite: {
+        type: String,
+        default: "",
+      },
+      companyLogo: {
+        type: String, // Cloudinary URL
+        default: "",
+      },
+      industry: {
+        type: String,
+        required: true,
+      },
+      description: {
+        type: String,
+        maxlength: 1000,
+      },
+      foundedYear: {
+        type: Number,
+      },
+      
+})
+
+module.exports = mongoose.model("Company", companySchema);
