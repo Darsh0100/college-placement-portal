8b60f38c804f31dd9213b66b99b46337a426b024
YasharthTri <yasharth1312@gmail.com>
2026-06-16 15:21:51 +0530

    fixed issues

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 exports.auth=(req,res,next)=>{
     try{
         //extract JWT token
-        const token=req.body.token||req.cookie.token||req.header("Authorization").replace("Bearer"," ");
+        const token=req.body.token||req.cookies.token||req.header("Authorization").replace("Bearer"," ");
         if(!token){
             return res.status(401).json({
                 success:false,
@@ -13,8 +13,8 @@ exports.auth=(req,res,next)=>{
         }
         //verify token
         try{
-            const decode= jwt.verify(token,process.env.JWT_SECRET)
-            req.user=decode;
+            const decoded= jwt.verify(token,process.env.JWT_SECRET)
+            req.user=decoded;
 
         }catch(error){
             return res.status(500).json({
