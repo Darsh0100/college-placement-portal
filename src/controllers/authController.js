f398d2b224eab93ac828269e5a8dee5e28924cdc
Darsh0100 <darshtiwari71@gmail.com>
2026-06-16 14:05:47 +0530

    Added JWT authentication

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-
+const bcrypt = require("bcryptjs");
 const registerUser = async (req, res) => {
   try {
     const { name, email, password, role, branch, cgpa, skills, resumeUrl } =
@@ -13,10 +13,14 @@ const registerUser = async (req, res) => {
       });
     }
 
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
       role,
       branch,
       cgpa,
