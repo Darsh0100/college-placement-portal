98144d54caf2554bed3004a152a39a20b6db31f8
Darsh0100 <darshtiwari71@gmail.com>
2026-06-18 14:30:21 +0530

    completed company and job module

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,35 +2,52 @@ const express = require("express");
 
 const router = express.Router();
 
-const { registerUser,loginUser } = require("../controllers/authController");
-const {auth,isStudent,isRecruiter,isAdmin}=require("../middleware/authMiddleware");
+const { registerUser, loginUser } = require("../controllers/authController");
 
+const {
+  auth,
+  isStudent,
+  isRecruiter,
+  isAdmin,
+} = require("../middleware/authMiddleware");
+
+const { saveCompanyDetails } = require("../controllers/recruiterController");
 
 router.post("/register", registerUser);
-router.post("/login",loginUser);
+router.post("/login", loginUser);
 
 router.get("/register", (req, res) => {
   res.send("Register GET Route Working");
 });
 
-//protected routes
-router.get("/student",auth,isStudent,(req,res)=>{
+// protected routes
+
+router.get("/student", auth, isStudent, (req, res) => {
   res.status(200).json({
-      success:true,
-      message:"access to students",
-  })
-})
-router.get("/admin",auth,isAdmin,(req,res)=>{
+    success: true,
+    message: "access to students",
+  });
+});
+
+router.get("/recruiter", auth, isRecruiter, (req, res) => {
   res.status(200).json({
-      success:true,
-      message:"access to Admin",
-  })
-})
-router.get("/admin",auth,isRecruiter,(req,res)=>{
+    success: true,
+    message: "access to recruiter",
+  });
+});
+
+router.get("/admin", auth, isAdmin, (req, res) => {
   res.status(200).json({
-      success:true,
-      message:"access to recruiter",
-  })
-})
+    success: true,
+    message: "access to Admin",
+  });
+});
+
+router.post(
+  "/recruiter/company-details",
+  auth,
+  isRecruiter,
+  saveCompanyDetails
+);
 
 module.exports = router;
