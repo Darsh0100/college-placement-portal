d507cc6ff68592e535685bfe92aa5662ad6f3640
YasharthTri <yasharth1312@gmail.com>
2026-06-19 16:11:54 +0530

    job routes updated

--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { auth, isRecruiter } = require("../middleware/authMiddleware");
+const { auth, isRecruiter} = require("../middleware/authMiddleware");
 
 const {
   createJob,
